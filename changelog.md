85b1eb8ce33d91e721642ab0fa86c633c7d2dd1e
M336 <75208577+M336G@users.noreply.github.com>
2026-03-08 14:58:18 +0100

    v1.1.0

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# 1.1.0
+- Badges are now resized automatically (instead of sometimes having a duplicate resized texture)
+- Codebase improvements
+
 # 1.0.1
 - Fixed badge(s) being duplicated on reload
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,87 @@
 #include <Geode/modify/ProfilePage.hpp>
 
+using namespace geode::prelude;
+
 class $modify(MyProfilePage, ProfilePage) {
+private:
     struct BadgeInfo {
-        const char *sprite;
-        int priority;
-        cocos2d::SEL_MenuHandler callback;
+        char const* sprite;
+        int const priority;
+        float const scale; // 92.f (badge resolution) / <highest between width or height>
+        SEL_MenuHandler const callback;
     };
 
+public:    
     struct Fields {
-        const std::unordered_map<std::string, BadgeInfo> m_badges = {
+        std::unordered_map<std::string_view, BadgeInfo> const m_badges = {
             { "star-collector", {
-                "SB_starBadge_001.png"_spr,
+                "GJ_bigStar_noShadow_001.png",
                 60,
+                92.f / 164.f,
                 menu_selector(MyProfilePage::onStarCollectorBadge)
             } },
             { "moon-collector", {
-                "SB_moonBadge_001.png"_spr,
+                "GJ_bigMoon_noShadow_001.png",
                 50,
+                92.f / 142.f,
                 menu_selector(MyProfilePage::onMoonCollectorBadge)
             } },
             { "secret-coin-collector", {
                 "GJ_coinsIcon_001.png",
                 70,
+                1.f,
                 menu_selector(MyProfilePage::onGoldCoinCollectorBadge)
             } },
             { "user-coin-collector", {
                 "GJ_coinsIcon2_001.png",
                 80,
+                1.f,
                 menu_selector(MyProfilePage::onSilverCoinCollectorBadge)
             } },
             { "demon-slayer", {
-                "SB_demonBadge_003.png"_spr,
+                "diffIcon_10_btn_001.png",
                 20,
+                92.f / 160.f,
                 menu_selector(MyProfilePage::onDemonSlayerBadge)
             } },
             { "demon-master", {
-                "SB_demonBadge_002.png"_spr,
+                "diffIcon_06_btn_001.png",
                 40,
+                92.f / 134.f,
                 menu_selector(MyProfilePage::onDemonMasterBadge)
             } },
             { "demon-chaser", {
-                "SB_demonBadge_001.png"_spr,
+                "diffIcon_07_btn_001.png",
                 90,
+                92.f / 132.f,
                 menu_selector(MyProfilePage::onDemonChaserBadge)
             } },
             { "creator", {
-                "SB_creatorBadge_001.png"_spr,
+                "GJ_hammerIcon_001.png",
                 30,
+                92.f / 100.f,
                 menu_selector(MyProfilePage::onCreatorBadge)
             } }
         };
 
-        cocos2d::CCMenu *m_usernameMenu;
+        CCMenu* m_usernameMenu;
     };
 
-    void addProfileBadge(std::string type) {
-        auto [sprite, priority, callback] = m_fields->m_badges.at(type);
-        auto id = fmt::format("{}-badge:{}"_spr, type, priority);
+    void addProfileBadge(std::string_view type) {
+        auto& [spriteName, priority, scale, callback] = m_fields->m_badges.at(type);
+        auto const id = fmt::format("{}-badge:{}"_spr, type, priority);
 
         // If it already exists don't add it again
-        if (static_cast<CCMenuItemSpriteExtra *>(
+        if (static_cast<CCMenuItemSpriteExtra*>(
             m_fields->m_usernameMenu->getChildByIDRecursive(id)))
             return;
 
-        auto *badge = CCMenuItemSpriteExtra::create(
-            cocos2d::CCSprite::createWithSpriteFrameName(sprite),
+        auto* sprite = CCSprite::createWithSpriteFrameName(spriteName);
+        if (scale != 1.f)
+            sprite->setScale(scale);            
+
+        auto* badge = CCMenuItemSpriteExtra::create(
+            sprite,
             this,
             callback
         );
@@ -73,17 +90,16 @@ class $modify(MyProfilePage, ProfilePage) {
         m_fields->m_usernameMenu->addChild(badge);
     };
 
-    void loadPageFromUserInfo(GJUserScore *score) {
+    void loadPageFromUserInfo(GJUserScore* score) {
         ProfilePage::loadPageFromUserInfo(score);
 
         // Get the menu to add the badges to
-        auto *layer = m_mainLayer;
-        m_fields->m_usernameMenu = static_cast<cocos2d::CCMenu *>(
-            layer->getChildByIDRecursive("username-menu"));
+        m_fields->m_usernameMenu = static_cast<CCMenu*>(
+            m_mainLayer->getChildByIDRecursive("username-menu"));
         if (!m_fields->m_usernameMenu) return;
 
         // This is just to make the mod badge first
-        auto *modBadge = static_cast<cocos2d::CCSprite *>(
+        auto* modBadge = static_cast<CCSprite*>(
             m_fields->m_usernameMenu->getChildByIDRecursive("mod-badge"));
         if (modBadge) modBadge->setID("mod-badge:10");
 
@@ -101,56 +117,56 @@ class $modify(MyProfilePage, ProfilePage) {
         m_fields->m_usernameMenu->updateLayout();
     };
 
-    void onStarCollectorBadge(CCObject *) {
+    void onStarCollectorBadge(CCObject*) {
         FLAlertLayer::create(
             "Star collector!",
             "This player has collected over <cy>3,000 stars</c>!",
             "OK"
         )->show();
     };
-    void onMoonCollectorBadge(CCObject *) {
+    void onMoonCollectorBadge(CCObject*) {
         FLAlertLayer::create(
             "Moon collector!",
             "This player has collected over <cl>3,000 moons</c>!",
             "OK"
         )->show();
     };
-    void onGoldCoinCollectorBadge(CCObject *) {
+    void onGoldCoinCollectorBadge(CCObject*) {
         FLAlertLayer::create(
             "Secret coin collector!",
             "This player has collected over <co>100 secret coins</c>!",
             "OK"
         )->show();
     };
-    void onSilverCoinCollectorBadge(CCObject *) {
+    void onSilverCoinCollectorBadge(CCObject*) {
         FLAlertLayer::create(
             "User coin collector!",
             "This player has collected over <cj>1,000 user coins</c>!",
             "OK"
         )->show();
     };
-    void onDemonSlayerBadge(CCObject *) {
+    void onDemonSlayerBadge(CCObject*) {
         FLAlertLayer::create(
             "Demon slayer!",
             "This player has completed over <cr>100 demon levels</c>!",
             "OK"
         )->show();
     };
-    void onDemonMasterBadge(CCObject *) {
+    void onDemonMasterBadge(CCObject*) {
         FLAlertLayer::create(
             "Demon master!",
             "This player has completed over <cr>50 demon levels</c>!",
             "OK"
         )->show();
     };
-    void onDemonChaserBadge(CCObject *) {
+    void onDemonChaserBadge(CCObject*) {
         FLAlertLayer::create(
             "Demon chaser!",
             "This player has completed over <cr>25 demon levels</c>!",
             "OK"
         )->show();
     };
-    void onCreatorBadge(CCObject *) {
+    void onCreatorBadge(CCObject*) {
         FLAlertLayer::create(
             "Creator badge!",
             "This player has been awarded with <cb>creator points</c> by <cb>RobTop</c>!",
